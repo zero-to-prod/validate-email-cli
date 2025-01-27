74ceef7372dd15c787f938e83c0c97cfc9d05efd
david_smith <david_smith@sweetwater.com>
2025-01-27 07:29:35 -0500

    feat/cli Created `tests/Unit/ValidateCommandTest.php`.

---
+++ tests/Unit/ValidateCommandTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\Attributes\Test;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use Tests\TestCase;
+use Zerotoprod\ValidateEmailCli\SrcCommand;
+
+class ValidateCommandTest extends TestCase
+{
+    #[Test] public function command(): void
+    {
+        $Application = new Application();
+        $Application->add(new SrcCommand());
+
+        $Command = $Application->find('validate-email-cli:src');
+        $CommandTester = new CommandTester($Command);
+        $CommandTester->execute([]);
+
+        $CommandTester->assertCommandIsSuccessful();
+    }
+}
