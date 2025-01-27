34196f7f29fbf9c822380c3aebab79cbceaa5d29
david_smith <david_smith@sweetwater.com>
2025-01-27 07:17:47 -0500

    feat/cli Updated `src/SrcCommand.php`.

--- src/SrcCommand.php
+++ src/SrcCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Zerotoprod\:package_namespace;
+namespace Zerotoprod\ValidateEmailCli;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
-    name: ':package_slug:src',
+    name: 'validate-email-cli:src',
     description: 'Project source link'
 )]
 class SrcCommand extends Command
@@ -16,7 +16,7 @@ class SrcCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeLn('https://github.com/zero-to-prod/:package_slug');
+        $output->writeLn('https://github.com/zero-to-prod/validate-email-cli');
 
         return Command::SUCCESS;
     }
