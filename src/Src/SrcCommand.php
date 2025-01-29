707e9cf1993d8870ce2c5e88b46f5b7aaf0fbc32
david_smith <david_smith@sweetwater.com>
2025-01-29 14:35:30 -0500

    main Renamed `src/SrcCommand.php` to `src/Src/SrcCommand.php`.

--- src/Src/SrcCommand.php
+++ src/Src/SrcCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Zerotoprod\ValidateEmailCli;
+namespace Zerotoprod\ValidateEmailCli\Src;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
